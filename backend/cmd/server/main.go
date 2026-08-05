1900cc7b915628251c2e6c95ed0ff094529a4243
Tiago Antolagic Duarte <duarte.tiago@web.de>
2026-08-05 17:09:07 +0200

    added cors middleware

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -25,8 +25,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	r := router.Setup()
+	handler := corsMiddleware(r)
 
 	log.Println("Server läuft auf :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
